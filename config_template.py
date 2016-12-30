db3589e93de08484a3daa88e2fa6665d9823d882
lawrencexia <xialairei@gmail.com>
2016-12-29 16:48:43 -0800

    update config template

--- config_template.py
+++ config_template.py
@@ -10,8 +10,4 @@ WTF_CSRF_ENABLED = True
 
 # GOOGLE OAUTH CONFIG
 CLIENT_SECRET_FILE = 'client_secret.json'
-G_REDIRECT_URI     = 'https://localhost:5000/gCallback'
-G_AUTH_URI         = 'https://accounts.google.com/o/oauth2/auth'
-G_TOKEN_URI        = 'https://accounts.google.com/o/oauth2/token'
-G_USER_INFO        = 'https://www.googleapis.com/userinfo/v2/me'
 G_SCOPE            = 'https://www.googleapis.com/auth/gmail.readonly'
